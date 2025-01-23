b8f7222e56eaea8c3259ed601ee7a308fa3fcd26
小周同学 <93928779+xiaozhou-tx@users.noreply.github.com>
2025-01-23 17:22:14 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-	res.render("index", { title: "后台管理系统" });
+	res.render("index", { title: "便捷开发系统" });
 });
 
 module.exports = router;
